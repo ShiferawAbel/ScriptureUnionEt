7f47fef013e5d1154b7c7dceb6442dbd3a6f94a5
Abela777 <abelshiferaw69@gmail.com>
2024-09-04 17:56:04 +0300

    added delete button to admin.videos.show

--- resources/views/admin/videos/show.blade.php
+++ resources/views/admin/videos/show.blade.php
@@ -2,4 +2,9 @@
     {!! $video->youtube_iframe !!}
     {{$video->title}}
     <a href="{{ route('admin.videos.edit', $video) }}">Edit</a>
+    <form action="{{ route('admin.videos.destroy', $event) }}" method="post">
+        @csrf
+        @method('delete')
+        <button type="submit" class="donate-link">DELETE</button>
+    </form>
 </x-layouts.admin>
