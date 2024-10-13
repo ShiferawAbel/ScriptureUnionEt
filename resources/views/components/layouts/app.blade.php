db721dc5f883b495a606a5b6b0cb54a575fdfc05
Abela777 <abelshiferaw69@gmail.com>
2024-10-13 22:06:09 +0300

    in hurry

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -9,13 +9,12 @@
     <meta content="" name="description">
 
     <!-- Favicon -->
-    <link rel="icon" href="{{asset('su-logo.png')}}" type="image/icon type">
-
+    
     <!-- Google Web Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&family=Roboto:wght@500;700&display=swap" rel="stylesheet">
-
+    
     <!-- Icon Font Stylesheet -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
@@ -29,6 +28,7 @@
     
     <!-- Template Stylesheet -->
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+    <link rel="icon" href="{{asset('img/su-logo.png')}}" type="image/icon type">
 </head>
 
 <body>
