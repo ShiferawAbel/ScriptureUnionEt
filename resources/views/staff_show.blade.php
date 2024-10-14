989b9aaed8abca68eea87c7912ae05e95566e82f
Abela777 <abelshiferaw69@gmail.com>
2024-10-15 00:58:15 +0300

    small change

--- resources/views/staff_show.blade.php
+++ resources/views/staff_show.blade.php
@@ -5,6 +5,6 @@
     <p>First Name: {{$staff->first_name}}</p>
     <p>Last Name: {{$staff->last_name}}</p>
     <p>Role At SU: {{$staff->role}}</p>
-    {!! QrCode::size(200)->format('png')->generate(url()->current()) !!}
+    <img src="data:image/png;base64,{!! base64_encode(QrCode::size(200)->format('png')->generate(url()->current())) !!}" alt="QR Code">
   </div>
 </x-layouts.app>
