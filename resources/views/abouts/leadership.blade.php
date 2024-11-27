1b58d477e704cba4d8cd14b75bb93780170ce176
Eyasu Mesele <106749746+12joshua@users.noreply.github.com>
2024-11-27 14:04:06 +0300

    board
    members

--- resources/views/abouts/leadership.blade.php
+++ resources/views/abouts/leadership.blade.php
@@ -21,13 +21,16 @@
                     </div>
                 </div>
                 <div class="col-md-4 row leader-card align-items-center">
-                    <img src="{{ asset('img/berhanu.jpg') }}" alt="Dr. Matewos Trsite" class="img-fluid text-center">
+                    <img src="{{ asset('img/Mr. Berhanu Solomon [Secretary].png') }}" alt="Dr. Matewos Trsite"
+                        class="img-fluid text-center">
                     <div class="leader-info text-center">
-                        <h4 class="text-center">Mr. Berhanu Solomon G/Michael</h4>
+                        <h4 class="text-center">Mr. Berhanu Solomon</h4>
+                        <h6 class="text-center">Board Secretary</h6>
                     </div>
                 </div>
                 <div class="col-md-4 row leader-card align-items-center">
-                    <img src="{{ asset('img/yonatan.jpg') }}" alt="Dr. Yonatan Z Amanueal" class="img-fluid text-center">
+                    <img src="{{ asset('img/yonatan.jpg') }}" alt="Dr. Yonatan Z Amanueal"
+                        class="img-fluid text-center">
                     <div class="leader-info text-center">
                         <h4 class="text-center">Dr. Yonatan Z Amanueal</h4>
                     </div>
@@ -44,7 +47,7 @@
                         <h4 class="text-center">Mrs. Truwork Ayele</h4>
                     </div>
                 </div>
-                <div class="col-md-4 row leader-card align-items-center">
+                <div class="col-md-4 row leader-card align-items-center ">
                     <img src="{{ asset('img/eyerusalem.jpg') }}" alt="Mrs. Eyerusalem Hile" class="img-fluid text-center">
                     <div class="leader-info text-center">
                         <h4 class="text-center">Mrs. Eyerusalem Hile</h4>
@@ -54,6 +57,49 @@
                     <img src="{{ asset('img/aframe.jpg') }}" alt="Pastor Aframe Mola" class="img-fluid text-center">
                     <div class="leader-info text-center">
                         <h4 class="text-center">Pastor Aframe Mola</h4>
+                    </div>
+                </div>
+                <div class="col-md-4 row leader-card align-items-center">
+                    <img src="{{ asset('img/Dr. Wendmu Kena.png') }}" alt="Pastor Aframe Mola"
+                        class="img-fluid text-center">
+                    <div class="leader-info text-center">
+                        <h4 class="text-center">Dr. Wendmu Kena</h4>
+                    </div>
+                </div>
+            </div>
+            <div class="leadership-title">
+                <h2>Managment</h2>
+                <p>Meet Our Managment Of Scripture Union Of Ethiopia</p>
+            </div>
+            <div class="row">
+                <div class="col-md-4 row leader-card align-items-center">
+                    <img src="{{ asset('img/Mr. Berhanu Solomon [National Director]2.png') }}" alt="Mr. Negussie Bulcha" class="img-fluid text-center">
+                    <div class="leader-info text-center">
+                        <h4 class="text-center">Mr. Berhanu Solomon</h4>
+                        <h6 class="text-center">National Director</h6>
+                    </div>
+                </div>
+                <div class="col-md-4 row leader-card align-items-center">
+                    <img src="{{ asset('img/Dr.Matewos Tirsitewold [School Ministry Director]2.png') }}" alt="Dr. Surafel Mulatu" class="img-fluid text-center">
+                    <div class="leader-info text-center">
+                        <h4 class="text-center">Dr.Matewos Tirsitewold</h4>
+                        <h6 class="text-center">School Ministry Director</h6>
+                    </div>
+                </div>
+                <div class="col-md-4 row leader-card align-items-center">
+                    <img src="{{ asset('img/Mr. Natnael Zeleke [Media and Communication Director]2.png') }}" alt="Dr. Matewos Trsite"
+                        class="img-fluid text-center">
+                    <div class="leader-info text-center">
+                        <h4 class="text-center">Mr. Natnael Zeleke</h4>
+                        <h6 class="text-center">Media and Communication Director</h6>
+                    </div>
+                </div>
+                <div class="col-md-4 row leader-card align-items-center">
+                    <img src="{{ asset('img/Mr. Abel Fisseha [Staff Capacity Development Director]2.png') }}" alt="Dr. Yonatan Z Amanueal"
+                        class="img-fluid text-center">
+                    <div class="leader-info text-center">
+                        <h4 class="text-center">Mr. Abel Fisseha</h4>
+                        <h6 class="text-center">Staff Capacity Development Director</h6>
                     </div>
                 </div>
             </div>
